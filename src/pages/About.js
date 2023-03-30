b828aca086ae4eb419bc4e708c0de1d67a9bb594
G-Alves1 <2kgabrielalves@gmail.com>
2023-03-30 10:25:08 -0300

    style(css): Tailwind in About page

--- src/pages/About.js
+++ src/pages/About.js
@@ -17,8 +17,8 @@ class About extends React.Component {
     return (
       <div>
         <Header />
-        <div>
-          <div>
+        <div className="flex mx-12 mt-6 text-1.1rem">
+          <div className="text-center mr-1 w-3/6 leading-1.6rem">
             <p>Olá 👋, Eu sou o Gabriel Alves</p>
             <p>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
@@ -41,64 +41,66 @@ class About extends React.Component {
               facilisis feugiat lacinia.
             </p>
           </div>
-          <div>
-            <img src="" alt="Minha foto" />
+          <div className="text-center ml-1 w-3/6">
+            <img src="" alt="Minha foto" className="w-full" />
           </div>
         </div>
-        <hr />
-        <div>
-          <h1>Habilidades</h1>
-          <div>
-            <p>- Linguagens -</p>
-            <div>
+        <hr className="w-30% my-9 mx-auto text-color-#ccc" />
+        <div className="text-center mb-28 py-2 w-3/5 m-auto bg-color-#282A2C bg-cover shadow-black bg-opacity-70">
+          <h1 className="text-4xl font-bold mb-2 font-albertson tracking-wider">
+            Habilidades
+          </h1>
+          <div className="mb-6">
+            <p className="text-1.2rem mb-4">- Linguagens -</p>
+            <div className="flex justify-center flex-wrap">
               {languages.map((stack) => (
                 <Stack key={stack.id} badge={stack.badge} name={stack.name} />
               ))}
             </div>
           </div>
-          <div>
-            <p>- Frameworks & Libraries -</p>
-            <div>
+          <div className="mb-6">
+            <p className="text-1.2rem mb-4">- Frameworks & Libraries -</p>
+            <div className="flex justify-center flex-wrap">
               {frameworkLibrary.map((stack) => (
                 <Stack key={stack.id} badge={stack.badge} name={stack.name} />
               ))}
             </div>
           </div>
-          <div>
-            <p>- Testes -</p>
-            <div>
+          <div className="mb-6">
+            <p className="text-1.2rem mb-4">- Testes -</p>
+            <div className="flex justify-center flex-wrap">
               {testing.map((stack) => (
                 <Stack key={stack.id} badge={stack.badge} name={stack.name} />
               ))}
             </div>
           </div>
-          <div>
-            <p>- Banco de Dados -</p>
-            <div>
+          <div className="mb-6">
+            <p className="text-1.2rem mb-4">- Banco de Dados -</p>
+            <div className="flex justify-center flex-wrap">
               {dataBase.map((stack) => (
                 <Stack key={stack.id} badge={stack.badge} name={stack.name} />
               ))}
             </div>
           </div>
-          <div>
-            <p>- ORM -</p>
-            <div>
+          <div className="mb-6">
+            <p className="text-1.2rem mb-4">- ORM -</p>
+            <div className="flex justify-center flex-wrap">
               {orm.map((stack) => (
                 <Stack key={stack.id} badge={stack.badge} name={stack.name} />
               ))}
             </div>
           </div>
-          <div>
-            <p>- Nuvem -</p>
-            <div>
+          <div className="mb-6">
+            <p className="text-1.2rem mb-4">- Nuvem -</p>
+            <div className="flex justify-center flex-wrap">
               {cloud.map((stack) => (
                 <Stack key={stack.id} badge={stack.badge} name={stack.name} />
               ))}
             </div>
           </div>
-          <div>
-            <p>- Outros -</p>
-            <div>
+          <div className="mb-6">
+            <p className="text-1.2rem mb-4">- Outros -</p>
+            <div className="flex justify-center flex-wrap">
               {others.map((stack) => (
                 <Stack key={stack.id} badge={stack.badge} name={stack.name} />
               ))}
