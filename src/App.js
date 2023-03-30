754f7deb5fd5e174bcd519a75fa9c2b6b4d9c5c1
G-Alves1 <2kgabrielalves@gmail.com>
2023-03-30 10:30:40 -0300

    feat(): Route Projects in App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
+import Projects from "./pages/Projects";
 import "./css/Header.css";
 
 class App extends React.Component {
@@ -10,6 +11,7 @@ class App extends React.Component {
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route exact path="/about" element={<About />} />
+        <Route exact path="/projects" element={<Projects />} />
       </Routes>
     );
   }
