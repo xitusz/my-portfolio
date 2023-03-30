7c3acc7725c74122c5e59b27817c732c32366f90
G-Alves1 <2kgabrielalves@gmail.com>
2023-03-30 10:31:44 -0300

    style(css): Tailwind in Projects page

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -15,10 +15,12 @@ class Projects extends React.Component {
     return (
       <div>
         <Header />
-        <div>
-          <div>
-            <h1>Projetos pessoais</h1>
-            <div>
+        <div className="text-center mx-auto mb-28 mt-6">
+          <div className="mb-12">
+            <h1 className="text-4xl font-bold font-albertson tracking-wider">
+              Projetos pessoais
+            </h1>
+            <div className="flex flex-nowrap snap-x overflow-x-auto m-auto w-4/5">
               {personal.map((project) => (
                 <Project
                   key={project.id}
@@ -33,21 +35,22 @@ class Projects extends React.Component {
               ))}
             </div>
           </div>
-          <hr />
+          <hr className="w-3/6 mx-auto my-6 items-center text-color-#ccc" />
           <div>
-            <h1>
+            <h1 className="text-4xl font-bold mb-6 font-albertson tracking-wider">
               Projetos desenvolvidos na{" "}
               <a
                 href="https://www.betrybe.com/"
                 target="_blank"
                 rel="noreferrer"
+                className="text-color-#ffdc84 no-underline hover:text-color-#ffba05"
               >
                 Trybe
               </a>
             </h1>
-            <div>
-              <h2>- Fundamentos -</h2>
-              <div>
+            <div className="mb-12">
+              <h2 className="text-2xl font-bold">- Fundamentos -</h2>
+              <div className="flex flex-nowrap snap-x overflow-x-auto m-auto w-4/5">
                 {fundamentals.map((project) => (
                   <Project
                     key={project.id}
@@ -62,9 +65,9 @@ class Projects extends React.Component {
                 ))}
               </div>
             </div>
-            <div>
-              <h2>- Front End -</h2>
-              <div>
+            <div className="mb-12">
+              <h2 className="text-2xl font-bold">- Front End -</h2>
+              <div className="flex flex-nowrap snap-x overflow-x-auto m-auto w-4/5">
                 {frontEnd.map((project) => (
                   <Project
                     key={project.id}
@@ -79,9 +82,9 @@ class Projects extends React.Component {
                 ))}
               </div>
             </div>
-            <div>
-              <h2>- Back End -</h2>
-              <div>
+            <div className="mb-12">
+              <h2 className="text-2xl font-bold">- Back End -</h2>
+              <div className="flex flex-nowrap snap-x overflow-x-auto m-auto w-4/5">
                 {backEnd.map((project) => (
                   <Project
                     key={project.id}
@@ -96,9 +99,9 @@ class Projects extends React.Component {
                 ))}
               </div>
             </div>
-            <div>
-              <h2>- Ciências da Computação -</h2>
-              <div>
+            <div className="mb-12">
+              <h2 className="text-2xl font-bold">- Ciências da Computação -</h2>
+              <div className="flex flex-nowrap snap-x overflow-x-auto m-auto w-4/5">
                 {computerScience.map((project) => (
                   <Project
                     key={project.id}
