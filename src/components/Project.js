2e21951e56e8ff8fceb63fd61a310cbb14d99941
G-Alves1 <2kgabrielalves@gmail.com>
2023-03-30 10:31:15 -0300

    style(css): Tailwind in Project component

--- src/components/Project.js
+++ src/components/Project.js
@@ -5,20 +5,25 @@ import ModalImage from "react-modal-image";
 class Project extends React.Component {
   render() {
     const { name, image, info, about, github, deployed, page } = this.props;
-
     return (
-      <div>
-        <p>{name}</p>
+      <div className="flex flex-col items-center snap-start scroll-ml-6 my-6 mx-6 pb-6 rounded min-w-30% bg-color-#282A2C bg-cover shadow-black bg-opacity-50">
+        <p className="text-1.2rem font-bold p-4">{name}</p>
         <ModalImage
+          className="w-full mb-4"
           small={image}
           large={image}
           alt={name}
           hideDownload="false"
           hideZoom="false"
         />
-        <div>
+        <div className="flex justify-evenly w-full">
           {info ? <p>{about}</p> : <></>}
-          <a href={github} target="_blank" rel="noreferrer">
+          <a
+            href={github}
+            target="_blank"
+            rel="noreferrer"
+            className="shadow-black p-1 rounded-20%"
+          >
             <img
               src="https://user-images.githubusercontent.com/25181517/192108374-8da61ba1-99ec-41d7-80b8-fb2f7c0a4948.png"
               alt="github"
