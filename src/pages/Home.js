c414d73e5642a3a9aa3969acc6cec9c51b1bcbb2
G-Alves1 <2kgabrielalves@gmail.com>
2023-03-30 09:51:51 -0300

    feat(pages): Home

---
+++ src/pages/Home.js
@@ -0,0 +1,44 @@
+import React from "react";
+
+class Home extends React.Component {
+  render() {
+    return (
+      <div>
+        <div>
+          <div>
+            <p>GABRIEL</p>
+            <p>ALVES</p>
+          </div>
+          <div>
+            <p>Desenvolvedor Web Full Stack</p>
+          </div>
+          <hr />
+          <div>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+              facilisis feugiat lacinia.
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+              facilisis feugiat lacinia.
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+              facilisis feugiat lacinia.
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+              facilisis feugiat lacinia.
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+              facilisis feugiat lacinia.
+            </p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Home;
