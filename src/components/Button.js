5245ca2a3bc41e2c9a0b45904b125c8593d4cab1
G-Alves1 <2kgabrielalves@gmail.com>
2023-04-01 07:30:36 -0300

    style(css): Tailwind in Button component

--- src/components/Button.js
+++ src/components/Button.js
@@ -6,7 +6,11 @@ class Button extends React.Component {
     const { children, onClick } = this.props;
     return (
       <div>
-        <button type="button" onClick={onClick}>
+        <button
+          type="button"
+          className="border border-solid border-color-#5c4200 shadow-black py-2 px-3 mx-3 rounded-full bg-color-#c45911 hover:bg-color-#ffba05 font-bold active:bg-color-#ffdc84"
+          onClick={onClick}
+        >
           {children}
         </button>
       </div>
