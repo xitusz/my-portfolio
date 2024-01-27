47995a592b926a9584d5f91cea3d880e910263ff
G-Alves1 <2kgabrielalves@gmail.com>
2024-01-27 15:38:51 -0300

    refactor(): migrate to functional component in App.js and import Routes

--- src/App.js
+++ src/App.js
@@ -1,20 +1,8 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Projects from "./pages/Projects";
-import "./css/Header.css";
+import Routes from "./routes";
 
-class App extends React.Component {
-  render() {
-    return (
-      <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route exact path="/about" element={<About />} />
-        <Route exact path="/projects" element={<Projects />} />
-      </Routes>
-    );
-  }
-}
+const App = () => {
+  return <Routes />;
+};
 
 export default App;
