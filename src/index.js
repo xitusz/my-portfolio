91c9320b5904ff576ac4d56a2e641ba0b79eb20d
G-Alves1 <2kgabrielalves@gmail.com>
2024-01-29 04:44:14 -0300

    chore(): change HashRouter to BrowserRouter in index.js

--- src/index.js
+++ src/index.js
@@ -2,17 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import reportWebVitals from "./reportWebVitals";
 import "./css/Header.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <HashRouter>
-      <App />
-    </HashRouter>
-  </React.StrictMode>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
