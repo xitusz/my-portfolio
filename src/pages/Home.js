bcbbaf22f340a054a2261d092d5eb739370f7c28
G-Alves1 <2kgabrielalves@gmail.com>
2024-01-29 04:50:28 -0300

    chore(pages): update import in Home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Header from "../components/Header.jsx";
+import Header from "../components/Header";
 import Footer from "../components/Footer";
 
 const Home = () => {
