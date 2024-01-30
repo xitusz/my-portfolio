cd389eca02ef3afdc5b744fb1302ac981b21bea3
G-Alves1 <2kgabrielalves@gmail.com>
2024-01-30 18:23:42 -0300

    feat(data): personal project 1

--- src/data/personalProjects.js
+++ src/data/personalProjects.js
@@ -1,3 +1,15 @@
-const personalProject = [];
+const personalProject = [
+  {
+    id: 1,
+    name: "Personal Project 1",
+    image:
+      "https://raw.githubusercontent.com/xitusz/personal-project-1/main/readme-images/region.gif",
+    info: false,
+    about: "",
+    github: "https://github.com/xitusz/personal-project-1",
+    deployed: false,
+    page: "",
+  },
+];
 
 export default personalProject;
