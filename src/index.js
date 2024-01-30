47fe0b97914677600debed99bf71bbf79ec6fc3b
G-Alves1 <2kgabrielalves@gmail.com>
2024-01-30 18:22:42 -0300

    chore(): import Button.css in index.js

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import reportWebVitals from "./reportWebVitals";
 import "./css/Header.css";
+import "./css/Button.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
