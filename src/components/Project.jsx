708fa7f16ca996d550aa7c637e688afe7316aa6e
G-Alves1 <2kgabrielalves@gmail.com>
2024-01-30 17:23:32 -0300

    style(components): border, background image, shadow-black and icons in Project

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import PropTypes from "prop-types";
 import ModalImage from "react-modal-image";
+import { FaGithub } from "react-icons/fa";
+import { GrDeploy } from "react-icons/gr";
+import { BsFillInfoCircleFill } from "react-icons/bs";
 
 const Project = ({ name, image, info, about, github, deployed, page }) => {
   return (
-    <div className="flex flex-col items-center snap-start scroll-ml-6 my-6 mx-6 pb-6 rounded min-w-30% bg-color-#282A2C bg-cover shadow-black bg-opacity-70">
-      <p className="text-1.2rem font-bold p-4">{name}</p>
+    <div className="flex flex-col items-center snap-start scroll-ml-6 my-6 mx-6 pb-6 rounded border-solid border-8 border-background-50 bg-cover bg-background2 shadow-black min-w-460px">
+      <p className="text-1.2rem font-bold pt-4 px-4">{name}</p>
+      <hr className="w-3/6 mx-auto shadow-black border-solid border-2 border-background-50" />
       <ModalImage
-        className="w-full mb-4"
+        className="w-full my-4 border-solid border-y-4 border-background-50"
         small={image}
         large={image}
         alt={name}
@@ -15,22 +19,47 @@ const Project = ({ name, image, info, about, github, deployed, page }) => {
         hideZoom="false"
       />
       <div className="flex justify-evenly w-full">
-        {info ? <p>{about}</p> : <></>}
+        {info ? (
+          <a
+            href={about}
+            target="_blank"
+            rel="noreferrer"
+            className="p-1 rounded-20% border-solid border-4 border-background-50 bg-cover bg-background2 shadow-black"
+          >
+            <BsFillInfoCircleFill
+              size={26}
+              className="text-white icon hover:text-color-#ffdc84 active:text-color-#ffba05 hover:scale-110 duration-200"
+              data-testid="info-icon"
+            />
+          </a>
+        ) : (
+          <></>
+        )}
         <a
           href={github}
           target="_blank"
           rel="noreferrer"
-          className="shadow-black p-1 rounded-20%"
+          className="p-1 rounded-20% border-solid border-4 border-background-50 bg-cover bg-background2 shadow-black"
         >
-          <img
-            src="https://user-images.githubusercontent.com/25181517/192108374-8da61ba1-99ec-41d7-80b8-fb2f7c0a4948.png"
-            alt="github"
-            height="50px"
-            width="50px"
+          <FaGithub
+            size={26}
+            className="text-white icon hover:text-color-#ffdc84 active:text-color-#ffba05 hover:scale-110 duration-200"
+            data-testid="github-icon"
           />
         </a>
         {deployed ? (
-          <a href={page} target="_blank" rel="noreferrer"></a>
+          <a
+            href={page}
+            target="_blank"
+            rel="noreferrer"
+            className="p-1 rounded-20% border-solid border-4 border-background-50 bg-cover bg-background2 shadow-black"
+          >
+            <GrDeploy
+              size={26}
+              className="text-white icon hover:text-color-#ffdc84 active:text-color-#ffba05 hover:scale-110 duration-200"
+              data-testid="deployed-icon"
+            />
+          </a>
         ) : (
           <></>
         )}
