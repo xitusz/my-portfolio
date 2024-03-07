81ada1e39cf0f54bfc4594dfd6e4a387bb86cea5
G-Alves1 <2kgabrielalves@gmail.com>
2024-03-07 16:50:01 -0300

    chore(data): update name and link from personal project 1 to LoL explorer

--- src/data/personalProjects.js
+++ src/data/personalProjects.js
@@ -3,11 +3,11 @@ import img from "../assets/images";
 const personalProject = [
   {
     id: 1,
-    name: "Personal Project 1",
+    name: "LoL Explorer",
     image: img.project1,
     info: false,
     about: "",
-    github: "https://github.com/xitusz/personal-project-1",
+    github: "https://github.com/xitusz/lol-explorer-js-mysql-react",
     deployed: false,
     page: "",
   },
